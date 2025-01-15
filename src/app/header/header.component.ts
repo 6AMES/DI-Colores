d45fac809b280602d500e93ca6c453596fb08fec
Hernani <victor.heralb@gmail.com>
2025-01-15 12:50:24 +0100

    0.10

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    imports: [CommonModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    activeButton: string | null = null;
 
+    constructor() {}
+
+    ngOnInit(): void {
+        this.setActive('m-1');
+    }
+
+    setActive(buttonId: string): void {
+        this.activeButton = buttonId;
+    }
 }
